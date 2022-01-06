1c7554a8e2a0c157a7db4c8080c6773c8f64a493
fnellmeldin <f.nellmeldin@open-engineering.com>
2022-01-06 19:39:38 +0100

    use StatefulWidget instead of StatelessWidget and implement setState(() {});

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,24 +16,39 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 3;
+
+  void generateNewNumbers() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(children: [
         Expanded(
           child: OutlinedButton(
-            child: Image.asset('images/dice1.png'),
+            child: Image.asset('images/dice$leftDiceNumber.png'),
             onPressed: () {
-              print('Left button');
+              generateNewNumbers();
             },
           ),
         ),
         Expanded(
           child: OutlinedButton(
-            child: Image.asset('images/dice2.png'),
+            child: Image.asset('images/dice$rightDiceNumber.png'),
             onPressed: () {
-              print('Right button');
+              generateNewNumbers();
             },
           ),
         ),
