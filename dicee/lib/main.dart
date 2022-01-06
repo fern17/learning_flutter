e2599e3be675ee622ad6d1652c5cde9dde312e22
fnellmeldin <f.nellmeldin@open-engineering.com>
2022-01-06 17:11:30 +0100

    add buttons and first callbacks (print)

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -18,13 +18,25 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-      Expanded(
-        child: Image.asset('images/dice1.png'),
-      ),
-      Expanded(
-        child: Image.asset('images/dice2.png'),
-      ),
-    ]);
+    return Center(
+      child: Row(children: [
+        Expanded(
+          child: OutlinedButton(
+            child: Image.asset('images/dice1.png'),
+            onPressed: () {
+              print('Left button');
+            },
+          ),
+        ),
+        Expanded(
+          child: OutlinedButton(
+            child: Image.asset('images/dice2.png'),
+            onPressed: () {
+              print('Right button');
+            },
+          ),
+        ),
+      ]),
+    );
   }
 }
