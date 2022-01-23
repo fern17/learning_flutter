a3c96642f78a2f22989c7b62c3717d9a769460c3
Fernando Nellmeldin <fernando.nellmeldin@gmail.com>
2022-01-23 12:20:28 +0100

    toggle checkbox state

--- todoey/lib/widgets/task_tile.dart
+++ todoey/lib/widgets/task_tile.dart
@@ -1,14 +1,48 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  void checkboxCallback(bool? checkboxState) {
+    setState(() {
+      isChecked = checkboxState ?? true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('task 1'),
-      trailing: Checkbox(
-        onChanged: (value) {},
-        value: false,
+      title: Text(
+        'task 1',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
+      trailing: TaskCheckbox(
+        checkboxState: isChecked,
+        toggleCheckboxState: checkboxCallback,
+      ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final Function(bool?) toggleCheckboxState;
+
+  TaskCheckbox(
+      {required this.checkboxState, required this.toggleCheckboxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      onChanged: toggleCheckboxState,
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
     );
   }
 }
