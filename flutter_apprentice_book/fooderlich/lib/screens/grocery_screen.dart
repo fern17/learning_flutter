c2b373ae78b006fc5d765bb42c44f50b58bf6a89
Fernando Nellmeldin <fernando.nellmeldin@gmail.com>
2023-03-22 11:55:10 +0100

    add empty groceries list screen

--- flutter_apprentice_book/fooderlich/lib/screens/grocery_screen.dart
+++ flutter_apprentice_book/fooderlich/lib/screens/grocery_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-
+import 'empty_grocery_screen.dart';
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.green);
+    return const EmptyGroceryScreen();
   }
 }
