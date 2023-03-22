e8fa1cf5ded2cc9670575ec506e61f510a7e5121
Fernando Nellmeldin <fernando.nellmeldin@gmail.com>
2023-03-22 10:54:16 +0100

    add a nested list view

--- flutter_apprentice_book/fooderlich/lib/screens/explore_screen.dart
+++ flutter_apprentice_book/fooderlich/lib/screens/explore_screen.dart
@@ -15,8 +15,21 @@ class ExploreScreen extends StatelessWidget {
       future: mockService.getExploreData(),
       builder: (context, AsyncSnapshot<ExploreData> snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
-          final recipes = snapshot.data?.todayRecipes ?? [];
-          return TodayRecipeListView(recipes: recipes);
+          return ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              TodayRecipeListView(
+                recipes: snapshot.data?.todayRecipes ?? [],
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Container(
+                height: 400,
+                color: Colors.green,
+              ),
+            ],
+          );
         } else {
           return const Center(
             child: CircularProgressIndicator(),
