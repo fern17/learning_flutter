bafe06f903cfa93e753a498ee83e70be4699ecac
Fernando Nellmeldin <fernando.nellmeldin@gmail.com>
2023-03-22 12:49:07 +0100

    add initial grocery item screen

--- flutter_apprentice_book/fooderlich/lib/screens/grocery_screen.dart
+++ flutter_apprentice_book/fooderlich/lib/screens/grocery_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'empty_grocery_screen.dart';
+import 'grocery_item_screen.dart';
 import '../models/models.dart';
 
 class GroceryScreen extends StatelessWidget {
@@ -12,7 +13,21 @@ class GroceryScreen extends StatelessWidget {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          final manager = Provider.of<GroceryManager>(context, listen: false);
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => GroceryItemScreen(
+                onCreate: (item) {
+                  manager.addItem(item);
+                  Navigator.pop(context);
+                },
+                onUpdate: (item) {},
+              ),
+            ),
+          );
+        },
       ),
       body: buildGroceryScreen(),
     );
