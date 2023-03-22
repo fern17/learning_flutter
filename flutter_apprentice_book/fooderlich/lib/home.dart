afaca23d819ea7063e2f962a5541630cbcca7579
Fernando Nellmeldin <fernando.nellmeldin@gmail.com>
2023-03-22 11:46:14 +0100

    started working in the grocery screen

--- flutter_apprentice_book/fooderlich/lib/home.dart
+++ flutter_apprentice_book/fooderlich/lib/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'screens/explore_screen.dart';
 import 'screens/recipes_screen.dart';
+import 'screens/grocery_screen.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -16,7 +17,7 @@ class HomeState extends State<Home> {
   static List<Widget> pages = <Widget>[
     ExploreScreen(),
     RecipesScreen(),
-    Container(color: Colors.blue),
+    const GroceryScreen(),
   ];
 
   void _onItemTapped(int index) {
