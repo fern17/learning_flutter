f644782c2db1f45731f16f7028f71e2120ad9ad4
Fernando Nellmeldin <fernando.nellmeldin@gmail.com>
2023-05-12 19:38:07 +0200

    fix issues with onboarding screen

--- flutter_apprentice_book/fooderlich/lib/navigation/app_router.dart
+++ flutter_apprentice_book/fooderlich/lib/navigation/app_router.dart
@@ -10,94 +10,93 @@ class AppRouter {
   AppRouter(this.appStateManager, this.profileManager, this.groceryManager);
 
   late final router = GoRouter(
-      debugLogDiagnostics: true, // TODO: this should be removed in production
-      refreshListenable: appStateManager,
-      initialLocation: '/login',
-      routes: [
-        GoRoute(
-          name: 'home',
-          path: '/:tab',
-          builder: (context, state) {
-            final tab = int.tryParse(state.params['tab'] ?? '') ?? 0;
-            return Home(key: state.pageKey, currentTab: tab);
-          },
-          routes: [
-            GoRoute(
-              name: 'item',
-              path: 'item/:id',
-              builder: (context, state) {
-                final itemId = state.params['id'] ?? '';
-                final item = groceryManager.getGroceryItem(itemId);
-                return GroceryItemScreen(
-                  originalItem: item,
-                  onCreate: (item) {
-                    groceryManager.addItem(item);
-                  },
-                  onUpdate: (item) {
-                    groceryManager.updateItem(item);
-                  },
-                );
-              },
-            ),
-            GoRoute(
-              name: 'profile',
-              path: 'profile',
-              builder: (context, state) {
-                final tab = int.tryParse(state.params['tab'] ?? '') ?? 0;
-                return ProfileScreen(
-                  user: profileManager.getUser,
-                  currentTab: tab,
-                );
-              },
-              routes: [
-                GoRoute(
-                  name: 'rw',
-                  path: 'rw',
-                  builder: (context, state) => const WebViewScreen(),
-                ),
-              ],
-            ),
-          ],
-        ),
-        GoRoute(
-          name: 'login',
-          path: '/login',
-          builder: (context, state) => const LoginScreen(),
-        ),
-        GoRoute(
-          name: 'onboarding',
-          path: '/onboarding',
-          builder: (context, state) => const OnboardingScreen(),
-        ),
-      ],
-      errorPageBuilder: (context, state) {
-        return MaterialPage(
-          key: state.pageKey,
-          child: Scaffold(
-            body: Center(
-              child: Text(
-                state.error.toString(),
+    debugLogDiagnostics: true, // TODO: this should be removed in production
+    refreshListenable: appStateManager,
+    initialLocation: '/login',
+    routes: [
+      GoRoute(
+        name: 'login',
+        path: '/login',
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        name: 'onboarding',
+        path: '/onboarding',
+        builder: (context, state) => const OnboardingScreen(),
+      ),
+      GoRoute(
+        name: 'home',
+        path: '/:tab',
+        builder: (context, state) {
+          final tab = int.tryParse(state.params['tab'] ?? '') ?? 0;
+          return Home(key: state.pageKey, currentTab: tab);
+        },
+        routes: [
+          GoRoute(
+            name: 'item',
+            path: 'item/:id',
+            builder: (context, state) {
+              final itemId = state.params['id'] ?? '';
+              final item = groceryManager.getGroceryItem(itemId);
+              return GroceryItemScreen(
+                originalItem: item,
+                onCreate: (item) {
+                  groceryManager.addItem(item);
+                },
+                onUpdate: (item) {
+                  groceryManager.updateItem(item);
+                },
+              );
+            },
+          ),
+          GoRoute(
+            name: 'profile',
+            path: 'profile',
+            builder: (context, state) {
+              final tab = int.tryParse(state.params['tab'] ?? '') ?? 0;
+              return ProfileScreen(
+                user: profileManager.getUser,
+                currentTab: tab,
+              );
+            },
+            routes: [
+              GoRoute(
+                name: 'rw',
+                path: 'rw',
+                builder: (context, state) => const WebViewScreen(),
               ),
+            ],
+          ),
+        ],
+      ),
+    ],
+    redirect: (state) {
+      final loggedIn = appStateManager.isLoggedIn;
+      final loggingIn = state.subloc == '/login';
+      if (!loggedIn) {
+        return loggingIn ? null : '/login';
+      }
+
+      final isOnboardingComplete = appStateManager.isOnboardingComplete;
+      final onboarding = state.subloc == '/onboarding';
+      if (!isOnboardingComplete) {
+        return onboarding ? null : '/onboarding';
+      }
+
+      if (loggingIn || onboarding) return '/${FooderlichTab.explore}';
+      return null;
+    },
+    errorPageBuilder: (context, state) {
+      return MaterialPage(
+        key: state.pageKey,
+        child: Scaffold(
+          body: Center(
+            child: Text(
+              state.error.toString(),
             ),
           ),
-        );
-      },
-      redirect: (state) {
-        final loggedIn = appStateManager.isLoggedIn;
-        final loggingIn = state.subloc == '/login';
-        if (!loggedIn) {
-          return loggingIn ? null : '/login';
-        }
-        /*
-        commented for now because it doesn't work...
-        final isOnBoardingComplete = appStateManager.isOnboardingComplete;
-        final onboarding = state.subloc == '/onboarding';
-        if (!isOnBoardingComplete) {
-          return onboarding ? null : '/onboarding';
-        }
-        */
-
-        if (loggingIn || false) return '/${FooderlichTab.explore}';
-        return null;
-      });
+        ),
+      );
+    },
+  );
 }
