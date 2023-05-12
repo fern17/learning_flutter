aad58cb70dd8d25ab30afc75b46481c8ad58e8a5
Fernando Nellmeldin <fernando.nellmeldin@gmail.com>
2023-05-12 18:28:34 +0200

    redirect to onboarding

--- flutter_apprentice_book/fooderlich/lib/navigation/app_router.dart
+++ flutter_apprentice_book/fooderlich/lib/navigation/app_router.dart
@@ -10,9 +10,47 @@ class AppRouter {
   AppRouter(this.appStateManager, this.profileManager, this.groceryManager);
 
   late final router = GoRouter(
-    debugLogDiagnostics: true, // TODO: this should be removed in production
-    refreshListenable: appStateManager,
-    initialLocation: '/login',
-    routes: [],
-  );
+      debugLogDiagnostics: true, // TODO: this should be removed in production
+      refreshListenable: appStateManager,
+      initialLocation: '/login',
+      routes: [
+        GoRoute(
+          name: 'login',
+          path: '/login',
+          builder: (context, state) => const LoginScreen(),
+        ),
+        GoRoute(
+          name: 'onboarding',
+          path: '/onboarding',
+          builder: (context, state) => const OnboardingScreen(),
+        ),
+      ],
+      errorPageBuilder: (context, state) {
+        return MaterialPage(
+          key: state.pageKey,
+          child: Scaffold(
+            body: Center(
+              child: Text(
+                state.error.toString(),
+              ),
+            ),
+          ),
+        );
+      },
+      redirect: (state) {
+        final loggedIn = appStateManager.isLoggedIn;
+        final loggingIn = state.subloc == '/login';
+        if (!loggedIn) {
+          return loggingIn ? null : '/login';
+        }
+
+        final isOnBoardingComplete = appStateManager.isOnboardingComplete;
+        final onboarding = state.subloc == '/onboarding';
+        if (!isOnBoardingComplete) {
+          return onboarding ? null : '/onboarding';
+        }
+
+        if (loggingIn || onboarding) return '/${FooderlichTab.explore}';
+        return null;
+      });
 }
