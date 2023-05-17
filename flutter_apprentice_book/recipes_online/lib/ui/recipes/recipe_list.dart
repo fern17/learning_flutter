c12e7c9999c120f29a59b249d11bec2886c0f986
Fernando Nellmeldin <fernando.nellmeldin@gmail.com>
2023-05-17 18:22:32 +0200

    finally show results

--- flutter_apprentice_book/recipes_online/lib/ui/recipes/recipe_list.dart
+++ flutter_apprentice_book/recipes_online/lib/ui/recipes/recipe_list.dart
@@ -30,12 +30,10 @@ class _RecipeListState extends State<RecipeList> {
   bool loading = false;
   bool inErrorState = false;
   List<String> previousSearches = <String>[];
-  APIRecipeQuery? _currentRecipes1;
 
   @override
   void initState() {
     super.initState();
-    loadRecipes();
 
     getPreviousSearches();
     searchTextController = TextEditingController(text: '');
@@ -63,13 +61,6 @@ class _RecipeListState extends State<RecipeList> {
     final recipeJson = await RecipeService().getRecipes(query, from, to);
     final recipeMap = json.decode(recipeJson);
     return APIRecipeQuery.fromJson(recipeMap);
-  }
-
-  Future loadRecipes() async {
-    final jsonString = await rootBundle.loadString('assets/recipes1.json');
-    setState(() {
-      _currentRecipes1 = APIRecipeQuery.fromJson(jsonDecode(jsonString));
-    });
   }
 
   @override
@@ -215,19 +206,46 @@ class _RecipeListState extends State<RecipeList> {
     );
   }
 
-  // TODO: Replace method
   Widget _buildRecipeLoader(BuildContext context) {
-    if (_currentRecipes1 == null || _currentRecipes1?.hits == null) {
+    if (searchTextController.text.length < 3) {
       return Container();
     }
-    return Flexible(
-      child: ListView.builder(
-          itemCount: 1,
-          itemBuilder: (BuildContext context, int index) {
-            return Center(
-                child: _buildRecipeCard(context, _currentRecipes1!.hits, 0));
-          }),
-    );
+
+    return FutureBuilder<APIRecipeQuery>(
+        future: getRecipeData(searchTextController.text.trim(),
+            currentStartPosition, currentEndPosition),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  snapshot.error.toString(),
+                  textAlign: TextAlign.center,
+                  textScaleFactor: 1.3,
+                ),
+              );
+            }
+            loading = false;
+            final query = snapshot.data;
+            inErrorState = false;
+            if (query != null) {
+              currentCount = query.count;
+              hasMore = query.more;
+              currentSearchList.addAll(query.hits);
+              if (query.to < currentEndPosition) {
+                currentEndPosition = query.to;
+              }
+            }
+            return _buildRecipeList(context, currentSearchList);
+          } else {
+            if (currentCount == 0) {
+              return const Center(child: CircularProgressIndicator());
+            } else {
+              return _buildRecipeList(
+                  context, currentSearchList); // show the current list
+            }
+          }
+        });
   }
 
   Widget _buildRecipeCard(
