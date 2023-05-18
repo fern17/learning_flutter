ffdec0dad75fbec1f75cbca0ff44e0b6f52f96d7
Fernando Nellmeldin <fernando.nellmeldin@gmail.com>
2023-05-18 18:34:20 +0200

    add generation of chopper part file

--- flutter_apprentice_book/recipes_online/lib/network/recipe_service.dart
+++ flutter_apprentice_book/recipes_online/lib/network/recipe_service.dart
@@ -3,6 +3,8 @@ import 'recipe_model.dart';
 import 'model_response.dart';
 import 'model_converter.dart';
 
+part 'recipe_service.chopper.dart'; // generated when running: flutter pub run build_runner build --delete-conflicting-outputs
+
 const String apiKey = '1da8792156917892a46004afd0cc4757';
 const String apiId = '168901c7';
 const String apiUrl = 'https://api.edamam.com';
@@ -12,12 +14,21 @@ abstract class RecipeService extends ChopperService {
   @Get(path: 'search')
   Future<Response<Result<APIRecipeQuery>>> queryRecipes(
       @Query('q') String query, @Query('from') int from, @Query('to') int to);
-  // TODO: Add create()
+  static RecipeService create() {
+    final client = ChopperClient(
+      baseUrl: Uri.parse(apiUrl),
+      interceptors: [_addQuery, HttpLoggingInterceptor()],
+      converter: ModelConverter(),
+      errorConverter: const JsonConverter(),
+      services: [_$RecipeService()],
+    );
+    return _$RecipeService(client);
+  }
 }
+
 Request _addQuery(Request req) {
   final params = Map<String, dynamic>.from(req.parameters);
   params['app_id'] = apiId;
   params['app_key'] = apiKey;
   return req.copyWith(parameters: params);
 }
-
