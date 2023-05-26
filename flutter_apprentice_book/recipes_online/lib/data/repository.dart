5fa6ebbe2cc3d46a54a88dc03a29945b0975b319
Fernando Nellmeldin <fernando.nellmeldin@gmail.com>
2023-05-26 20:06:15 +0200

    finished interface

--- flutter_apprentice_book/recipes_online/lib/data/repository.dart
+++ flutter_apprentice_book/recipes_online/lib/data/repository.dart
@@ -1,8 +1,19 @@
 import 'models/models.dart';
 
 abstract class Repository {
-  // TODO: add find methods
-  // TODO: add insert methods
-  // TODO: add delete methods
-  // TODO: add initializing and closing methods
+  List<Recipe> findAllRecipes();
+  Recipe findRecipeById(int id);
+  List<Ingredient> findAllIngredients();
+  List<Ingredient> findRecipeIngredients(int recipeId);
+
+  int insertRecipe(Recipe recipe);
+  List<int> insertIngredients(List<Ingredient> ingredients);
+
+  void deleteRecipe(Recipe recipe);
+  void deleteIngredient(Ingredient ingredient);
+  void deleteIngredients(List<Ingredient> ingredient);
+  void deleteRecipeIngredients(int recipeId);
+
+  Future init();
+  void close();
 }
